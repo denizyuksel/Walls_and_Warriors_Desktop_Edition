4186105f06cdb3049a89b2899d822e05fa9113a5
Deniz Yüksel <d.yuksel@ug.bilkent.edu.tr>
2018-11-19 15:28:11 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import gui.GUIManager;
+import javafx.application.Application;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Application.launch(GUIManager.class, args);
+	}
+
+}
